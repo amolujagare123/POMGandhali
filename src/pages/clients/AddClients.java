5d032b3887d41beea234500c1cd189409e9200c6
PC <amolujagare@gmail.com>
2020-07-07 20:24:29 +0530

    implemented the functionality of date selection

--- src/pages/clients/AddClients.java
+++ src/pages/clients/AddClients.java
@@ -1,76 +1,235 @@
 package pages.clients;
 
+import org.joda.time.DateTime;
+import org.joda.time.Months;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class AddClients {
 
 
-    @FindBy(xpath="//input[@id='client_name']")
-    WebElement txtClient;
+        @FindBy(xpath="//input[@id='client_name']")
+        WebElement txtClient;
 
-    @FindBy (xpath="//input[@id='client_surname']")
-    WebElement txtClientSurname;
+        @FindBy (xpath="//input[@id='client_surname']")
+        WebElement txtClientSurname;
 
-    @FindBy (xpath="//input[@id='client_address_1']")
-    WebElement txtStreetaddess1;
+        @FindBy (xpath="//input[@id='client_address_1']")
+        WebElement txtStreetaddess1;
 
-    @FindBy (xpath="//input[@id='client_address_2']")
-    WebElement txtStreetaddess2;
+        @FindBy (xpath="//input[@id='client_address_2']")
+        WebElement txtStreetaddess2;
 
-    @FindBy (xpath="//button[@name='btn_submit']")
-    WebElement btnSave;
+        @FindBy(xpath="//input[@id='client_city']")
+        WebElement txtCity;
+
+        @FindBy(xpath="//input[@id='client_state']")
+        WebElement txtState;
+
+        @FindBy(xpath="//input[@id='client_zip']")
+        WebElement txtZip;
+
+        @FindBy (id="select2-client_country-container")
+        WebElement containerCountry;
+
+        @FindBy(xpath="//input[@id='client_phone']")
+        WebElement txtPhone;
+
+        @FindBy(xpath="//input[@id='client_fax']")
+        WebElement txtFax;
+
+        @FindBy(xpath="//input[@id='client_mobile']")
+        WebElement txtMob;
+
+        @FindBy(xpath="//input[@id='client_email']")
+        WebElement txtEmail;
+
+        @FindBy(xpath="//input[@id='client_web']")
+        WebElement txtWeb;
+
+        @FindBy (id="select2-client_gender-container")
+        WebElement containerGender;
+
+        @FindBy(xpath="//input[@id='client_vat_id']")
+        WebElement txtVat;
+
+        @FindBy(xpath="//input[@id='client_tax_code']")
+        WebElement txtTaxes;
+
+        @FindBy (xpath="//button[@name='btn_submit']")
+        WebElement btnSave;
 
 
-    @FindBy (id="select2-client_language-container")
-    WebElement containerLanguage;
+        @FindBy (id="select2-client_language-container")
+        WebElement containerLanguage;
 
-    @FindBy (xpath = "//input[@type='search']")
-    WebElement txtSearch;
+        @FindBy (xpath = "//input[@type='search']")
+        WebElement txtSearch;
 
-    WebDriver driver;
+        WebDriver driver;
 
-    public void setlanguage(String language)
-    {
-        containerLanguage.click();
-        txtSearch.sendKeys(language);
-        driver.findElement(By.xpath("//li[contains(text(),'"+language+"')]")).click();
+        public void setlanguage(String language)
+        {
+            containerLanguage.click();
+            txtSearch.sendKeys(language);
+            driver.findElement(By.xpath("//li[contains(text(),'"+language+"')]")).click();
 
-    }
+        }
+
+    @FindBy (xpath="//input[@name='client_birthdate']")
+    WebElement txtBirthdate;
+
+        public void setBirthDate(String setDateStr) throws ParseException {
+            txtBirthdate.click();
+            // convert String into Date ------>    PSD
+
+            Date setDate = new SimpleDateFormat("dd/MM/yyyy").parse(setDateStr);
 
 
-    public AddClients(WebDriver driver)
-    {
-        this.driver = driver;
-        PageFactory.initElements(driver,this);
-    }
+            String currDateStr
+                    =driver.findElement(By.xpath("//th[@class='datepicker-switch']")).getText();
+
+            System.out.println(currDateStr);
+// date in MMMM yyyy
 
 
-    public void setTxtClient(String clientname)
-    {
-        txtClient.sendKeys(clientname);
-    }
+// convert String into Date ------>    PSD
 
-    public void setTxtClientSurname(String surname)
-    {
-        txtClientSurname.sendKeys(surname);
-    }
+            Date currDate = new SimpleDateFormat("MMMM yyyy").parse(currDateStr);
 
-    public void setTxtStreetaddess1(String address1)
-    {
-        txtStreetaddess1.sendKeys(address1);
-    }
 
-    public void setTxtStreetaddess2(String address2)
-    {
-        txtStreetaddess2.sendKeys(address2);
-    }
+            int monthDiff = Months.monthsBetween(new DateTime(currDate).withDayOfMonth(1),new DateTime(setDate).withDayOfMonth(1)).getMonths();
 
-    public void clickSave()
-    {
-        btnSave.click();
-    }
+            boolean isFuture=true;
+            if(monthDiff<0)
+            {
+                isFuture=false;
+                monthDiff=monthDiff*(-1);
+
+            }
+
+            for(int i=0;i<monthDiff;i++)
+            {
+                if(isFuture)
+                    driver.findElement(By.className("next")).click();
+                else
+                    driver.findElement(By.className("prev")).click();
+            }
+
+
+            String setDateday = new SimpleDateFormat("dd").format(setDate);
+
+           int setDateDayInt = Integer.parseInt(setDateday);
+
+           driver.findElement(By.xpath("//td[contains(text(),'"+setDateDayInt+"' ) and @class='day']")).click();
+
+
+        }
+
+
+        public AddClients(WebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.initElements(driver,this);
+        }
+
+
+        public void setTxtClient(String clientname)
+        {
+
+            txtClient.sendKeys(clientname);
+        }
+
+        public void setTxtClientSurname(String surname)
+        {
+
+            txtClientSurname.sendKeys(surname);
+        }
+
+        public void setTxtStreetaddess1(String address1)
+
+        {
+            txtStreetaddess1.sendKeys(address1);
+        }
+
+        public void setTxtStreetaddess2(String address2)
+        {
+
+            txtStreetaddess2.sendKeys(address2);
+        }
+        public void setTxtCity(String City)
+        {
+
+            txtCity.sendKeys(City);
+        }
+        public void setTxtState(String State)
+        {
+
+            txtState.sendKeys(State);
+        }
+        public void setTxtZip(String Zip)
+        {
+
+            txtZip.sendKeys(Zip);
+        }
+        public void setCountry(String Country)
+        {
+            containerCountry.click();
+            txtSearch.sendKeys(Country);
+            driver.findElement(By.xpath("//li[text()='"+Country+"']")).click();
+
+        }
+        public void setGender(String Gender)
+        {
+            containerGender.click();
+            //txtSearch.sendKeys(Gender);
+            driver.findElement(By.xpath("//li[contains(text(),'"+Gender+"')]")).click();
+
+        }
+        public void setTxtPhone(String Phone)
+        {
+
+            txtPhone.sendKeys(Phone);
+        }
+        public void setTxtFax(String Fax)
+        {
+
+            txtFax.sendKeys(Fax);
+        }
+        public void setTxtMob(String Mob)
+        {
+
+            txtMob.sendKeys(Mob);
+        }
+        public void setTxtEmail(String Email)
+        {
+
+            txtEmail.sendKeys(Email);
+        }
+        public void setTxtWeb(String Web)
+        {
+
+            txtWeb.sendKeys(Web);
+        }
+        public void setTxtVat(String Vat)
+        {
+
+            txtVat.sendKeys(Vat);
+        }
+        public void setTxtTaxes(String Tax)
+        {
+
+            txtTaxes.sendKeys(Tax);
+        }
+        public void clickSave()
+        {
+            btnSave.click();
+        }
 }
--- testsrc/Regression/clients/AddClientTest.java
+++ testsrc/Regression/clients/AddClientTest.java
@@ -5,12 +5,13 @@ import pages.Menu;
 import pages.clients.AddClients;
 import util.DoLogin;
 
+import java.text.ParseException;
+
 public class AddClientTest extends DoLogin {
 
 
     @Test
-    public void addClienttest()
-    {
+    public void addClienttest() throws ParseException {
         Menu menu = new Menu(driver);
         menu.clickAddClient();
 
@@ -20,8 +21,26 @@ public class AddClientTest extends DoLogin {
         addClients.setTxtClientSurname("Ujagare");
         addClients.setTxtStreetaddess1("Katraj");
         addClients.setTxtStreetaddess2("pune");
-        addClients.clickSave();*/
-
         addClients.setlanguage("Thai");
+        addClients.setTxtCity("Pune");
+        addClients.setTxtState("Maharastra");
+        addClients.setTxtZip("411046");
+        addClients.setCountry("India");
+        addClients.setTxtPhone("12345678");
+        addClients.setTxtFax("2222");
+        addClients.setTxtMob("7709492480");
+        addClients.setTxtEmail("bhorgandhali@gmail.com");
+        addClients.setTxtWeb("https://vritti.ekatm.com/");
+        addClients.setGender("Female");
+        addClients.setTxtVat("V123");
+        addClients.setTxtTaxes("40000");
+*/
+
+        addClients.setBirthDate("28/02/2019");
+
+       // addClients.clickSave();
+
+
+        // date selection : https://www.youtube.com/watch?v=yW1-IbI_soc
     }
 }
